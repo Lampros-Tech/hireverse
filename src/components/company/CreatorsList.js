69645900776e816587710b924471bfd234f5d6aa
Rahul Rajan <rahulrajan@lampros.tech>
2022-09-19 11:41:36 +0530

    merge some files

--- src/components/company/CreatorsList.js
+++ src/components/company/CreatorsList.js
@@ -1,94 +1,87 @@
-import React from "react";
+import React from 'react'
+import './styles/creatorlist.css'
 
 function CreatorsList() {
   return (
     <div>
-      <div classNameName="py-16 white gap-0.5">
-        <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6 ">
-          <h2 className="mb-12 text-center text-2xl text-gray-900 font-bold md:text-4xl ">
-            CREATORS
-          </h2>
-          <div className="grid gap-2 md:grid-rows-1 lg:grid-cols-1">
-          {/* ------------------------------------------code starts------------------------------------- */}
-          <div className="grid gap-2 md:grid-rows-1 lg:grid-cols-1">
-            <div className="p-6 border border-gray-100 rounded-xl bg-gray-50 sm:flex sm:space-x-6 sm:p-5">
-              <img
-                className="w-20 h-20 mx-auto rounded-full"
-                src="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/first_user.webp"
-                alt="user avatar"
-                height="220"
-                width="220"
-                loading="lazy"
-              />
-              <div className="space-y-4 mt-4 text-start sm:mt-0 sm:text-left">
-                <h6 className="text-lg font-semibold leading-none">Deepak</h6>
-                <p className="text-gray-600">
-                  {" "}
-                  <span className="font-serif">"</span> Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Quaerat repellat
-                  perspiciatis excepturi est. Non ipsum iusto aliquam
-                  consequatur repellat provident, omnis ut, sapiente voluptates
-                  veritatis cum deleniti repudiandae ad doloribus.{" "}
-                  <span className="font-serif">"</span>
-                </p>
-                <div className="flex-none flex items-center justify-center sm:justify-start  gap-10	">
-                  <div>
-                    <h5>
-                      <b>Created:</b>
-                    </h5>
-                    <h6>10 Tests</h6>
-                  </div>
-                  <div>
-                    <h5>
-                      <b>Industry experience:</b>
-                    </h5>
-                    <h6>3 Years</h6>
-                  </div>
-                </div>
-                <div className="flex-none flex items-center justify-center sm:justify-start  gap-10">
-                  <h5>
-                    <b>Expertise</b>
-                  </h5>
-                  <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:#134E6F dark:text-blue-800">
-                    Tag-1
-                  </span>
-                  <span class="bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-                    Tag-2
-                  </span>
-                  <span class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
-                    Tag-3
-                  </span>
-                </div>
-                <div className="flex-none flex items-end justify-end  gap-10">
-                  <button
-                    type="button"
-                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
+      <div className="cr-container">
+        <div className="cr-title">
+          <h1>Creators</h1>
+        </div>
+        <div className="cr-main">
+          <div className='cr-main-content'>
+            <img class="p-1 w-14 h-14 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="https://i.pravatar.cc/100" alt="Rounded avatar" />
+            <h1 className='cr-name'>Name OF The Creator</h1>
           </div>
-    
-        {/* ---------------------------------------------------------------code end--------------------------------------- */}
-        </div></div>
+          <div className="cr-row">
+            <div className="td-outer-heading">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+              </svg>
+
+
+              <span className="cr-head">Created: </span>
+              <span> XX tests</span>
+            </div>
+            <div className="td-outer-heading">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                <path fillRule="evenodd" d="M9.664 1.319a.75.75 0 01.672 0 41.059 41.059 0 018.198 5.424.75.75 0 01-.254 1.285 31.372 31.372 0 00-7.86 3.83.75.75 0 01-.84 0 31.508 31.508 0 00-2.08-1.287V9.394c0-.244.116-.463.302-.592a35.504 35.504 0 013.305-2.033.75.75 0 00-.714-1.319 37 37 0 00-3.446 2.12A2.216 2.216 0 006 9.393v.38a31.293 31.293 0 00-4.28-1.746.75.75 0 01-.254-1.285 41.059 41.059 0 018.198-5.424zM6 11.459a29.848 29.848 0 00-2.455-1.158 41.029 41.029 0 00-.39 3.114.75.75 0 00.419.74c.528.256 1.046.53 1.554.82-.21.324-.455.63-.739.914a.75.75 0 101.06 1.06c.37-.369.69-.77.96-1.193a26.61 26.61 0 013.095 2.348.75.75 0 00.992 0 26.547 26.547 0 015.93-3.95.75.75 0 00.42-.739 41.053 41.053 0 00-.39-3.114 29.925 29.925 0 00-5.199 2.801 2.25 2.25 0 01-2.514 0c-.41-.275-.826-.541-1.25-.797a6.985 6.985 0 01-1.084 3.45 26.503 26.503 0 00-1.281-.78A5.487 5.487 0 006 12v-.54z" clipRule="evenodd" />
+              </svg>
+
+              <span className="cr-head"> Industry Experiance: </span>
+              <span>3 Years</span>
+            </div>
+            
+
+          </div>
+          <div className="td-paragraph cr-pg">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt,
+              suscipit eum dolore nemo voluptatem reprehenderit assumenda nam
+              accusantium libero eveniet? Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Aliquam, distinctio?
+            </p>
+          </div>
+          <div className="cr-btn-main">
+            <div>
+              <h1 className='cr-name-tag'> Expertise :</h1>
+            </div>
+            <div className="tag-and-btn">
+              <button
+                type="button"
+                class=" tag-button px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-mdhover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                tag-1
+              </button>
+              <button
+                type="button"
+                class=" tag-button px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-mdhover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                tag-1
+              </button>
+              <button
+                type="button"
+                class=" tag-button px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-mdhover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                tag-1
+              </button>
+            </div>
+
+            <button type="button" class=" arreow-buttton text-white bg-blue-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center ">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clipRule="evenodd" />
+              </svg>
+
+              <span class="sr-only">Icon description</span>
+            </button>
+            
+            </div>
+        </div>
+      </div>
+
     </div>
-    </div>
-  );
+  )
 }
 
-export default CreatorsList;
+export default CreatorsList
