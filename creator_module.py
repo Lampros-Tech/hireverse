0e493db88eed572de4e61cd893c4b7a466f90e9c
Bhumi18 <Bhumi18.bhumisadariya18@gmail.com>
2022-09-20 10:48:51 +0530

    api updated

--- creator_module.py
+++ creator_module.py
@@ -52,50 +52,37 @@ def addTableNames():
         fields = "*"
         condition = f"""wallet_address = '{walletAddress}'"""
         creator_data = select_query(fields, tablename, condition)
-
         final_data = json.loads(creator_data.decode("utf-8"))
-        print(final_data)
-
         status_list = []
+        if final_data["rows"] == []:
+            return {"data": "not exists"}
+        else:
+            if data["question_table"]:
+                print(final_data["rows"][0][11])
+                if not final_data["rows"][0][11]:
+                    question_update_values = {"question_table": data["question_table"]}
+                    condition = f"""wallet_address='{walletAddress}'"""
+                    question_table = update_query(
+                        tablename, question_update_values, condition
+                    )
+                    print(question_table)
+                    status_list.append("Question table added successfully!!")
+                else:
+                    status_list.append("Question table already exists!!")
 
-        if data["question_table"]:
-            print(final_data["rows"][0][11])
-            if not final_data["rows"][0][11]:
-                question_update_values = {"question_table": data["question_table"]}
-                condition = f"""wallet_address='{walletAddress}'"""
-                question_table = update_query(
-                    tablename, question_update_values, condition
-                )
-                print(question_table)
-                status_list.append("Question table added successfully!!")
-            else:
-                status_list.append("Question table already exists!!")
+            if data["repo_table"]:
+                if not final_data["rows"][0][12]:
+                    repo_update_values = {"repo_table": data["repo_table"]}
+                    condition = f"""wallet_address='{walletAddress}'"""
+                    repo_table = update_query(tablename, repo_update_values, condition)
+                    print(repo_table)
+                    status_list.append("Repo table added successfully!!")
+                else:
+                    status_list.append("Repo table already exists!!")
 
-        if data["assesment_table"]:
-            if not final_data["rows"][0][12]:
-                assesment_update_values = {"assesment_table": data["assesment_table"]}
-                condition = f"""wallet_address='{walletAddress}'"""
-                assesment_table = update_query(
-                    tablename, assesment_update_values, condition
-                )
-                print(assesment_table)
-                status_list.append("Assesment table added successfully!!")
-            else:
-                status_list.append("Assesment table already exists!!")
-
-        if data["repo_table"]:
-            if not final_data["rows"][0][13]:
-                repo_update_values = {"repo_table": data["repo_table"]}
-                condition = f"""wallet_address='{walletAddress}'"""
-                repo_table = update_query(tablename, repo_update_values, condition)
-                print(repo_table)
-                status_list.append("Repo table added successfully!!")
-            else:
-                status_list.append("Repo table already exists!!")
-
-        # condition = f"""wallet_address='{walletAddress}' AND """
-        # data = update_query(tablename, update_values, condition, True)
-        return status_list, 200
+            # condition = f"""wallet_address='{walletAddress}' AND """
+            # data = update_query(tablename, update_values, condition, True)
+            return status_list, 200
     except Exception as e:
         print(e)
         return "Something went wrong !!", 500
@@ -119,13 +106,11 @@ def getTables():
         if final_data["rows"] == []:
             return {
                 "question_table": "none",
-                "assesment_table": "none",
                 "repo_table": "none",
             }
         else:
             creator_tables = {
                 "question_table": final_data["rows"][0][11],
-                "assesment_table": final_data["rows"][0][12],
                 "repo_table": final_data["rows"][0][12],
             }
             return creator_tables, 200
