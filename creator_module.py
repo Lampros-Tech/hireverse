34789c32bc9be4dad45454a98a77c9d9449af482
Akash <akash.lamprostech@gmail.com>
2022-09-24 01:05:17 +0530

    creat Assessment added

--- creator_module.py
+++ creator_module.py
@@ -6,7 +6,7 @@ import datetime
 import json
 from flask import Blueprint, render_template, abort
 from flask.globals import request
-
+import random
 load_dotenv()
 
 creator = Blueprint("creator", __name__)
@@ -121,6 +121,97 @@ def getTables():
     except Exception as e:
         print(e)
         return "Something went wrong !!", 500
+
+
+#creat Assessment API to create a test 
+@creator.route("/creator/createAssessment", methods=["POST"])
+def getAssessmentQuestions():
+    try:
+        #inputs
+        walletAddress = request.json["wallet_address"]
+        assessment_name = request.json['assessment_name']
+        description = request.json['description']
+        fixed_cost = request.json['fixed_cost']
+        variable_cost = request.json['variable_cost']
+        duration = request.json['duration']
+        number_of_questions = request.json['number_of_questions']
+        experience_level = request.json['experience_level']
+        primary_skills = request.json['primary_skills']
+        secondary_skills = request.json['secondary_skills']
+        question_format = request.json['question_format']
+
+        #selecting table of the creator
+        fields = "*"
+        tablename = os.environ.get("creators_table")
+        condition = f"""wallet_address = '{walletAddress}'"""
+        creator_data = select_query(fields, tablename, condition)
+        final_data = json.loads(creator_data.decode("utf-8"))
+        if final_data["rows"] == []:
+            return {
+                "msg": "Question Table is not Created",
+            },400
+        else:
+            #runnig query to select the questions
+            question_table= final_data["rows"][0][11]
+            creator_id = final_data["rows"][0][0]
+            question_list = []
+            for format in question_format:
+                fields = "*"
+                condition = f"""repo_name = '{format['repo_name']}' AND difficulty_level='{format['difficulty_level']}' AND primary_tag='{format['genre']}'"""
+                creator_questions = select_query(fields, question_table,condition)
+                questions = json.loads(creator_questions.decode("utf-8"))
+                num_questions = format['to']- format['from'] + 1 
+                if len(questions['rows']) >= num_questions:
+                    selected_questions = random.sample(questions['rows'],num_questions)
+                    for i in selected_questions:
+                        question_list.append(i[0])
+                    # print(questions['rows'])
+                else:
+                    response_body = {
+                        "status": 200,
+                        "data": format['repo_name'] + " repo does not have enough Questions for "+ format['genre'] +" with difficulty level "+ format['difficulty_level'] +"!",
+                    }, 200
+                    return response_body
+            repo_table_name = os.environ.get('creators_assesment_table')
+            fields = "(creators_id,title,description,fixed_cost,variable_cost,duration,question,number_of_question,created_at,experiance_level,primary_skill1,primary_skill2,primary_skill3,secondary_skills)"
+            try:
+                primary_skill1 = primary_skills[0]
+            except:
+                print("No primary Skill Available")    
+            try:
+                primary_skill2 = primary_skills[1]
+            except:
+                print("No primary Skill-1 Available")    
+            try:
+                primary_skill3 = primary_skills[2]
+            except:
+                print("No primary Skill-2 Available") 
+
+            print(len(question_list),number_of_questions)
+            created_at = int(datetime.datetime.now().timestamp())
+
+            if len(question_list) == number_of_questions:
+                values = f"""({creator_id},'{assessment_name}','{description}',{fixed_cost},{variable_cost},{duration},'{question_list}',{number_of_questions},{created_at},'{experience_level}','{primary_skill1}','{primary_skill2}','{primary_skill3}','{(',').join(secondary_skills)}')"""
+                data = insert_query(repo_table_name, fields, values)
+                response_body = {
+                    "status": 200,
+                    "data": "Assessment Created Successfully !",
+                }, 200
+
+            else:
+                response_body = {
+                    "status": 200,
+                    "data": "Not enough Questions found !"
+                }, 200
+
+
+        return response_body
+    except Exception as e:
+        print(e)
+        return "Something went wrong !!", 500
+
+
+
 
 
 ####################################################################################################################
